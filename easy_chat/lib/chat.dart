99b3d8743a5e350a4dd1f4aa1ee28a584fb27ee6
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2020-10-26 08:42:45 +0600

    store data in fire cloud storage and fixed simple bug

--- easy_chat/lib/chat.dart
+++ easy_chat/lib/chat.dart
@@ -1,7 +1,9 @@
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/services.dart';
 import 'constants.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class Chat extends StatefulWidget {
   static const id = 'chat_page';
@@ -12,7 +14,9 @@ class Chat extends StatefulWidget {
 
 class _ChatState extends State<Chat> {
   final FirebaseAuth auth = FirebaseAuth.instance;
+  final firestore=Firestore.instance;
   User logInUser;
+  String userMessage;
 
   getCurrentUser() {
     try {
@@ -44,21 +48,70 @@ class _ChatState extends State<Chat> {
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: GestureDetector(
-              onTap: (){
+              onTap: () {
                 auth.signOut();
                 SystemNavigator.pop();
                 showToast('Sign Out and Exit Successfully');
               },
-              child: Icon(Icons.backspace,
-              color: Colors.white,
+              child: Icon(
+                Icons.backspace,
+                color: Colors.white,
               ),
             ),
           )
         ],
       ),
       body: Container(
-        child: Column(
-          children: [],
+        child: ListView(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  flex: 6,
+                  child: TextField(
+                    scrollPadding: EdgeInsets.zero,
+                    textAlign:TextAlign.center,
+                    style: TextStyle(
+                      color: Colors.black87,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Type your message here',
+                      filled: true,
+                      fillColor: Color(0xFFEDEDED),
+                      contentPadding: EdgeInsets.only(top: 0,bottom: 0,left: 10,right: 0.0),
+                      border:OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30.0),
+                        ),
+
+                      ),
+                    ),
+                    onChanged: (value){
+                      userMessage=value;
+                    },
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: GestureDetector(
+                    onTap: (){
+                     // print(userMessage);
+                      firestore.collection('Messages').add(
+                        {
+                          'sender':logInUser.email,
+                          'information':userMessage,
+                        }
+                      );
+                    },
+                    child: Icon(
+                      Icons.send_sharp,
+                      color: Colors.black,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     ));
